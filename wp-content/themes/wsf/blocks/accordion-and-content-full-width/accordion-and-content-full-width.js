e7e00834b9a4a67ff7961713ad7ae5e0425e97fb
Prabhat501050 <prabhat.tripathi@annalect.com>
2024-05-10 23:12:38 +0530

    images are updated

--- wp-content/themes/wsf/blocks/accordion-and-content-full-width/accordion-and-content-full-width.js
+++ wp-content/themes/wsf/blocks/accordion-and-content-full-width/accordion-and-content-full-width.js
@@ -91,22 +91,6 @@ export const AccordionAndContentFullWidth = (() => {
 	});
 
 
-	// Accordian fadein Anmation  ==================================
-
-	// $(document).on("scroll", function () {
-	// 	var pageTop = $(document).scrollTop()
-	// 	var pageBottom = pageTop + $(window).height()
-	// 	var tags = $(".fadein")
-
-	// 	for (var i = 0; i < tags.length; i++) {
-	// 		var tag = tags[i]
-
-	// 		if ($(tag).offset().top < pageBottom) {
-	// 			$(tag).addClass("visible")
-	// 		} else {
-	// 			$(tag).removeClass("visible")
-	// 		}
-	// 	}
 	// })
 
 	return {
